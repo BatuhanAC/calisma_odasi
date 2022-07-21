a2cf332ba50970741b4dbc3051f11d4c4f5cf370
Batuhan Acar <batuhan.acar1@outlook.com>
2022-07-21 21:42:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Header from './components/macros/Header';
 import './index.css';
 import Main from './components/macros/Main'
+import Account from './components/macros/Account'
+import Login from './components/macros/Login'
+import Signup from './components/macros/Signup'
+import Session from './components/macros/Session'
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,7 +12,11 @@ function App() {
     <>
       <Header/>
       <Routes>
-        <Route path='/' element={Main} />
+        <Route path='/' element={<Main/>}/>
+        <Route path='Account' element={<Account/>} />
+        <Route path='Login' element={<Login/>} />
+        <Route path='Signup' element={<Signup/>} />
+        <Route path='Session' element={<Session/>} />
       </Routes>
     </>
   );
